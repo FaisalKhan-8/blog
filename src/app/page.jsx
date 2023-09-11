551099fafc117bb30753d535c3e1edccadfe57a4
Faisal Khan <md.fk@somaiya.edu>
2023-09-11 12:44:25 +0530

    sarter

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
-import styles from "./homepage.module.css";
+import Link from 'next/link';
+import styles from './homepage.module.css';
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href='/'></Link>
+    </div>
+  );
 }
